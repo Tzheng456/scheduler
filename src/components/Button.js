33515a39721c6d42845037022c32c1fb91cf38ef
Tzheng456 <Tzheng456@gmail.com>
2021-08-31 22:10:02 -0700

    add buttons and style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = 'button';
+
+  if (props.confirm) {
+    buttonClass += ' button--confirm';
+  }
+
+  if (props.danger) {
+    buttonClass += ' button--danger';
+  }
+
+  return (
+    <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
 }
