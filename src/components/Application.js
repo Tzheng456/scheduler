2efd4208b381dcc3474cfc4d399ab633b80f8561
Tzheng456 <Tzheng456@gmail.com>
2021-09-01 01:26:00 -0700

    update application with DayList inside nav element

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 import 'components/Application.scss';
+import DayList from './DayList';
 
 export default function Application(props) {
+  const days = [
+    {
+      id: 1,
+      name: 'Monday',
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: 'Tuesday',
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: 'Wednesday',
+      spots: 0,
+    },
+  ];
   return (
     <main className="layout">
       <section className="sidebar">
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList days={days} day={'Monday'} setDay={(day) => console.log(day)} />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
