7a57fd4cb4282ed9496bb9ab560465b3591b6bce
Tzheng456 <Tzheng456@gmail.com>
2021-09-01 00:16:42 -0700

    add function to handle spots string

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,19 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0,
   });
+
+  const formatSpots = () => {
+    let spotsString = '';
+    if (props.spots === 0) spotsString += 'no spots remaining';
+    if (props.spots === 1) spotsString += '1 spot remaining';
+    if (props.spots > 1) spotsString += `${props.spots} spots remaining`;
+    return spotsString;
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
