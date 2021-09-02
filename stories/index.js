c8fec3f803649c9c578b4992ac6f6d8552c45392
Tzheng456 <Tzheng456@gmail.com>
2021-09-01 17:54:02 -0700

    add Confirm and Show with stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf('Button', module)
   .addParameters({
@@ -124,4 +126,19 @@ storiesOf('Appointment', module)
   .add('Appointment', () => <Appointment />)
   .add('Appointment with Time', () => <Appointment time="12pm" />)
   .add('Header', () => <Header time="12pm" />)
-  .add('Empty', () => <Empty onClick={action('onAdd')} />);
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  ));
